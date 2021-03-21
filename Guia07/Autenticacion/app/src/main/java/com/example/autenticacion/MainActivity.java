5e9745567581388499ee5c30b96ce20cfa6c15ff
estela801 <ros98cg98@gmail.com>
2021-03-20 22:47:27 -0600

    capturas de funcionamiento

--- Guia07/Autenticacion/app/src/main/java/com/example/autenticacion/MainActivity.java
+++ Guia07/Autenticacion/app/src/main/java/com/example/autenticacion/MainActivity.java
@@ -12,17 +12,28 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 
@@ -31,12 +42,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText emailTV, passwordTV;
     private Button loginBtn;
     private Button Registro;
-    private Button BGoogle;
+    private SignInButton BGoogle;
+    private LoginButton BFb;
     private ProgressBar progressBar;
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
-    int RC_SIGN_IN=1;
-    String TAG= "GoogleSignIn";
+    private CallbackManager mCallbackManager;
+    int RC_SIGN_IN = 1;
+    String TAG = "GoogleSignIn";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +59,15 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         initializeUI();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
 //AUTENTICACION DE GOOGLE
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 //////////////////////////////////////////////////////
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -75,42 +91,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //AUTENTICACION CON FACEBOOK
+        mCallbackManager = CallbackManager.Factory.create();
+
+        BFb.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                Intent dashboardActivity = new Intent(MainActivity.this, Welcome.class);
+                startActivity(dashboardActivity);
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
 
 
-    }
-
-    //AUTENTICACION CON FACEBOOK
-
-// Initialize Facebook Login button
-    mCallbackManager = CallbackManager.Factory.create();
-    LoginButton loginButton = mBinding.buttonFacebookLogin;
-loginButton.setReadPermissions("email", "public_profile");
-loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            Log.d(TAG, "facebook:onSuccess:" + loginResult);
-            handleFacebookAccessToken(loginResult.getAccessToken());
-        }
-
-        @Override
-        public void onCancel() {
-            Log.d(TAG, "facebook:onCancel");
-            // ...
-        }
-
-        @Override
-        public void onError(FacebookException error) {
-            Log.d(TAG, "facebook:onError", error);
-            // ...
-        }
-    });
-    // ...
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass the activity result back to the Facebook SDK
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
@@ -129,7 +135,7 @@ loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -254,5 +260,6 @@ loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>
         BGoogle= findViewById(R.id.btnGoogle);
         loginBtn = findViewById(R.id.login);
         progressBar = findViewById(R.id.progressBar);
+        BFb= findViewById(R.id.btnFb);
     }
 }
